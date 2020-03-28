b2cae170adf3ff32f003e8953f00fccc9ff45565
odnakoholodno <61801404+odnakoholodno@users.noreply.github.com>
2020-03-28 15:17:39 +0300

    Update index.php

--- lw3/index.php
+++ lw3/index.php
@@ -1,25 +1,29 @@
-    <?php
-      header("Content-Type: text/plain");
-      function getGETParameter($parameter) {
-        return isset($_GET[$parameter]) ? $_GET[$parameter] : null;
-      };
-      $text = getGETParameter('text');
-
-      function removeSpacesOut($str) {
-        $i = 0;
-        while ($i < strlen($str) & $str[$i] == ' ') $i++;
-        while ($i < strlen($str)) {
-          if ($str[$i] != ' ') {
-            echo $str[$i];
-            $i++;
-          } else {
-            while ($str[$i] == ' ' & $i < strlen($str)) $i++;
-            if ($i != strlen($str)) echo ' ';
-          };
-        };
-      };
-
-    removeSpacesOut($text);
-    echo '#';
+<?php
+header("Content-Type: text/plain");
+function getGETParameter($parameter) 
+{
+  return isset($_GET[$parameter]) ? $_GET[$parameter] : null;
+}
+$text = getGETParameter('text');
+function removeSpacesOut($str) 
+{
+  $i = 0;
+  while ($i < strlen($str) & $str[$i] == ' ') $i++;
+  while ($i < strlen($str)) 
+  {
+    if ($str[$i] != ' ') 
+    {
+      echo $str[$i];
+      $i++;
+    } 
+    else 
+    {
+      while ($str[$i] == ' ' & $i < strlen($str)) $i++;
+      if ($i != strlen($str)) echo ' ';
+    }
+  }
+}
+removeSpacesOut($text);
+echo '#';
     
     
