88d4eb0f83567515df235d28efcb38b1229435ca
odnakoholodno <61801404+odnakoholodno@users.noreply.github.com>
2020-05-13 23:27:21 +0300

    Add files via upload

---
+++ lw2/lw24.pas
@@ -0,0 +1,32 @@
+PROGRAM WorkWithQueryString(INPUT, OUTPUT);
+USES
+  DOS;
+FUNCTION GetQueryStringParameter(Key: STRING): STRING;
+VAR
+  Query, NeededString: STRING;
+  Position: INTEGER;
+BEGIN
+  NeededString := '';
+  Query := GetEnv('QUERY_STRING');
+  Position := POS(Key + '=', Query);
+  IF (Position <> 0)
+  THEN
+    BEGIN
+      Position := Position + Length(Key) + 1;
+      WHILE ((Query[Position] <> '&') AND (Position <= Length(Query)))
+      DO
+        BEGIN
+          NeededString := NeededString + Query[Position];
+          Position := Position + 1
+        END;
+      GetQueryStringParameter := NeededString
+    END
+  ELSE
+    GetQueryStringParameter := 'not found'
+END;
+
+BEGIN {WorkWithQueryString}
+  WRITELN('First Name: ', GetQueryStringParameter('first_name'));
+  WRITELN('Last Name: ', GetQueryStringParameter('last_name'));
+  WRITELN('Age: ', GetQueryStringParameter('age'))
+END. {WorkWithQueryString}
