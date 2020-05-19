651c650ddb585896156ec12fdff5ca79ee386e9d
odnakoholodno <61801404+odnakoholodno@users.noreply.github.com>
2020-05-19 22:47:13 +0300

    Update index.php

--- lw3/index.php
+++ lw3/index.php
@@ -2,37 +2,37 @@
 header("Content-Type: text/plain");
 function getGETParameter($parameter) 
 {
-  return isset($_GET[$parameter]) ? $_GET[$parameter] : null;
+    return isset($_GET[$parameter]) ? $_GET[$parameter] : null;
 }
 $text = getGETParameter('text');
 if ($text == null)
 {
-  echo 'Вы не передали параметр';
+    echo 'Вы не передали параметр';
 }
 else
 {
-  echo removeExtraBlanks($text);
+    echo removeExtraBlanks($text);
 }
 function removeExtraBlanks(string $text): string
 {
-  $result = '';
-  $isBlank = true;
-  for ($i = 0; $i < strlen($text); $i++)
-  {
-    if ($text[$i] == ' ')
+    $result = '';
+    $isBlank = true;
+    for ($i = 0; $i < strlen($text); $i++)
     {
-      if ($isBlank == false)
-      {
-        $result .= ' ';
-        $isBlank = true;
-      }
+        if ($text[$i] == ' ')
+        {
+            if ($isBlank == false)
+            {
+                $result .= ' ';
+                $isBlank = true;
+            }
+        }
+        else
+        {
+            $result .= $text[$i];
+            $isBlank = false;
+        }
     }
-    else
-    {
-      $result .= $text[$i];
-      $isBlank = false;
-    }
-  }
-  return $result;
+    return $result;
 }
 
