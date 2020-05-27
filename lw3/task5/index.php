fee84a75e2c2ec90622c3b595347c6b3ea7709cd
odnakoholodno <61801404+odnakoholodno@users.noreply.github.com>
2020-05-28 02:18:06 +0300

    Add files via upload

---
+++ lw3/task5/index.php
@@ -0,0 +1,16 @@
+<?php
+header('Content-Type: text/plain');
+function getGETParameter($parameter)
+{
+    return isset($_GET[$parameter]) ? $_GET[$parameter] : null;
+}
+$email = getGETParameter('email');
+$filename = 'data/' . $email . '.txt';
+$fp = fopen($filename, 'r');
+$first_name = fgets($fp);
+$last_name = fgets($fp);
+$email = fgets($fp);
+$age = fgets($fp);
+$text = $first_name . $last_name . $email . $age;
+echo $text;
+?>
