cabee29f4c5a9ec907d90e841ac5afbe11d3fe12
odnakoholodno <61801404+odnakoholodno@users.noreply.github.com>
2020-05-28 09:37:12 +0300

    Update index.php

--- lw3/task3/index.php
+++ lw3/task3/index.php
@@ -4,20 +4,20 @@ function getGETParameter($parameter)
 {
     return isset($_GET[$parameter]) ? $_GET[$parameter] : null;
 }
-$identifier = getGETParameter('identifier');
-$len = strlen($identifier);
-if ($identifier == null || $len == 0)
+$password = getGETParameter('password');
+$len = strlen($password);
+if ($password == null || $len == 0)
 {
     echo 'Вы не передали параметр';
 }
 else
 {
-    echo passwordStrength($identifier);
+    echo passwordStrength($password);
 }
-function passwordStrength(string $identifier): int
+function passwordStrength(string $password): int
 {
     $strength = 0;
-    $len = strlen($identifier);
+    $len = strlen($password);
     $digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     $lowLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     $highLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
@@ -32,29 +32,29 @@ function passwordStrength(string $identifier): int
 
     for ($i = 0; $i < $len; $i++)
     {
-        if (in_array($identifier[$i], $digits))
+        if (in_array($password[$i], $digits))
         {
             $countDigits++;
         }
-        if (in_array($identifier[$i], $lowLetters))
+        if (in_array($password[$i], $lowLetters))
         {
             $countLow++;
         }
-        if (in_array($identifier[$i], $highLetters))
+        if (in_array($password[$i], $highLetters))
         {
             $countHigh++;
         }
-        if (!(stripos($withoutrepeat, $identifier[$i])))
+        if (!(stripos($withoutrepeat, $password[$i])))
         {
-            $withoutrepeat = $withoutrepeat . $identifier[$i];
+            $withoutrepeat = $withoutrepeat . $password[$i];
         }
     }
     $len2 = strlen($withoutrepeat);
     for ($i = 0; $i < $len2; $i++)
     {
-        if (substr_count($identifier, $withoutrepeat[$i]) > 1)
+        if (substr_count($password, $withoutrepeat[$i]) > 1)
         {
-          $strength = $strength - substr_count($identifier, $withoutrepeat[$i]);
+          $strength = $strength - substr_count($password, $withoutrepeat[$i]);
         }
     }
     if ($countDigits == 0)
